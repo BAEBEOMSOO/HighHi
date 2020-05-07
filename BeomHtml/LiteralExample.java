5dcd86ea95088ba7d784e890ed01d0ff4463697d
BAEBEOMSOO <qoqjatn707@naver.com>
2020-05-07 21:49:57 +0900

    05_07 방과후

---
+++ BeomHtml/LiteralExample.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class LiteralExample {
+
+	public static void main(String[] args) {
+		 	
+	//			기본형(Primitive type) : boolean, char, short, byte, int, long, float, double
+	//			참조형(Reference type) : 기본형을 제외한 나머지 !!!  String, System. 등			메모리주소저장..(부호없는정수)
+			
+	    // 기본형은 논리, 문자, 정수, 실수형으로 나뉜다.
+		// 논리형 - boolean,   문자형 -char  하나의 문자만을 저장
+		// 정수형 - int, long 등 정수 값을 저장하는데 사용. byte는 이진 데이터를 다루는데 사용
+		//실수형 - float, double 실수 값을 저장하는데 사용된다.
+	
+//		System.out.println(10/3);         // 정수 나누기 정수라서 정수 결과가 나옴
+//		System.out.println(10.0/3);     //  실수 나누기 정수를 해서 3.333333이 나옴.
+//		System.out.println(0x1A);    // 16진수 1A를 출력...
+		
+
+//		System.out.printf("%x%n", 15);
+//		System.out.printf("%o%n",55);
+//		System.out.printf("age : %d year : %d%n", 14, 2017);
+//		System.out.printf("%s", Integer.toBinaryString(1120));
+//		System.out.printf("%#x%n", 15);
+		
+		
+		
+//		double f = 123.456789;           //double 은 정밀도가 15자리라서 모두 정확하게 나옴!
+//		System.out.printf("%f%n",f)   ;
+//		System.out.printf("%e%n",f);
+//		System.out.printf("%g%n",f);
+	
+//		
+//		System.out.printf("[%5d]%n",1234567);
+//		System.out.printf("[%-5d]%n",10);
+//		System.out.printf("[%05d]%n",10);
+//	
+//		double d = 1.23456789;
+//		System.out.printf("%.6f", d);
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println(" ******* 입력 후 Tab키를 누르시면 편리하게 작성하실 수 있습니다.******");
+//		
+//		
+//		System.out.print("1. 주소:" );
+//		String address  = sc.nextLine();;
+//		System.out.println("제 주소는" + address + "입니다.");
+//		System.out.println(" ");
+//		System.out.print("2. 이름:" );
+//		String name  = sc.nextLine();;
+//		System.out.println("저의 이름은" + name + "입니다.");
+//		System.out.println(" ");
+//		
+//		System.out.print("3. 취미:");
+//		String hobby = sc.nextLine();
+//		System.out.println("저의 취미는" + hobby + "입니다. 같이 치실래요?");
+//		System.out.println(" ");
+//		
+//		System.out.print("4. 좋아하는 음식:");
+//		String FavoriteFood  = sc.nextLine();;
+//		System.out.println("제가 좋아하는 음식은" +FavoriteFood  + "입니다. 같이 드실래요?");
+//		System.out.println(" ");
+//		
+//		System.out.print("5. 휴대폰 번호 ('-'을 기입해주시기 바랍니다.):");
+//		String  phoneNumber = sc.nextLine();
+//		System.out.println("제 휴대폰 번호는" + phoneNumber + "입니다. ");
+//		System.out.println(" ");
+//		
+//		System.out.println("당신의 이름은:" + name + ", " + "거주지는:" + address + "," + "취미는:" + hobby + "," + "좋아하는 음식은:" + FavoriteFood + "," + "휴대폰 번호는: "  + phoneNumber + "입니다. ");
+//	
+//		sc.close();
+		
+		// ch2 - 17 타입간의 변환방법
+//		String str = "3";
+//		
+//		System.out.println(str.charAt(0) - '0');;  //3을출력
+//		System.out.println('3' - '0' +1); // 4를 출력
+//		System.out.println(Integer.parseInt("3") +1); // 4를 출력
+//		System.out.println("3" + "1");
+//		System.out.println(3 + '0'); // 51출력  0이 숫자로 48임
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+}
